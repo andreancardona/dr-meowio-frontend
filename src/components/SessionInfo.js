d5fd7a5f1f85ee1f2967e80bbb29241ecb3e7c63
johneckert <johnteckert@gmail.com>
2018-03-19 08:07:59 -0400

    fixed player color '

--- src/components/SessionInfo.js
+++ src/components/SessionInfo.js
@@ -5,7 +5,7 @@ const SessionInfo = props => {
     <div className="session-info-panel">
       <p className={`label ${props.currentTheme.name}-score`}>Score:</p>
       <p className={`score ${props.currentTheme.name}-score`}>{props.currentScore}</p>
-      <p className={`label ${props.currentTheme.name}-player`}>Player:</p>
+      <p className={`player-label label ${props.currentTheme.name}-player`}>Player:</p>
       <p className={`player ${props.currentTheme.name}-player`}>{props.currentUser.name}</p>
     </div>
   );
